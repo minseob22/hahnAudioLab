c94cf43a5b41cf974211c93a1a799d0bd1453d92
minseob22 <minseobhan22@gmail.com>
2024-06-19 00:21:40 +0900

    6.19 자기전 변경

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home'; 
-import Products from './pages/Products'; 
-import Packages from './pages/Packages'; 
-import Reviews from './pages/Reviews'; 
-import Blog from './pages/Blog'; 
-import Contact from './pages/Contact'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Packages from './pages/Packages';
+import Reviews from './pages/Reviews';
+import Blog from './pages/Blog';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route path="/products" component={Products} />
-        <Route path="/packages" component={Packages} />
-        <Route path="/reviews" component={Reviews} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/contact" component={Contact} />
-      </div>
-    </Router>
+    <div>
+      <header>
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#products">Products</a>
+          <a href="#packages">Packages</a>
+          <a href="#reviews">Reviews</a>
+          <a href="#blog">Blog</a>
+        </nav>
+      </header>
+      <main>
+        <Home />
+        <Products />
+        <Packages />
+        <Reviews />
+        <Blog />
+      </main>
+    </div>
   );
-};
+}
 
 export default App;
