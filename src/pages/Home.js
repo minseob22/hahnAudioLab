c92c6fc3ee45d167f8d0640254673c6f5e9b7713
minseob22 <minseobhan22@gmail.com>
2024-06-19 22:27:36 +0900

    인트로...?

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>홈 페이지</div>;
+  return (
+  <div className="visual_section">
+    <div className= "banner"> 
+      <img src={`${process.env.PUBLIC_URL}/img/62582a53409ff.jpg`} alt="Philosophy" />{/* 이미지 넣을 div */}
+        <div className="banner-txt"> {/* 텍스트 넣을 div */}
+        <h1> Hahn Audio Lab </h1> {/* 넣을 글자 */}
+        </div>
+      </div>
+
+      <div className="line">
+        <img>
+
+        </img>
+    </div>
+  
+</div>
+  )
 };
 
 export default Home;
