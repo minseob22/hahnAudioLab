640246ae2b628a3ea9ce28d9e1c4f8c26dc40a8b
minseob22 <minseobhan22@gmail.com>
2024-06-22 10:17:38 +0900

    철학 약간 변경

--- src/pages/Philosophy.js
+++ src/pages/Philosophy.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import './Philosophy.css';
-import { Form } from 'react-router-dom';
-
 
 const Philosophy = () => {
   const [texts, setTexts] = useState([]); // 입력된 문장을 저장할 상태
@@ -17,18 +15,20 @@ const Philosophy = () => {
       setInputText('');
     }
   };
-  
+
   return (
     <div className="philosophy-container">
-      <div className="philosophy-title">Philosophy</div>
+      <h2 className="philosophy-title">Philosophy</h2>
       <div className="philosophy-content">
         <div className="image-box">
-          {/* 사진 들어가는 상자 */}
           <img src={`${process.env.PUBLIC_URL}/img/27368be00be15.png`} alt="Philosophy" />
-
         </div>
         <div className="text-box">
-          {/* 글자 들어가는 상자 */}
+          <div className="text-container">
+            {texts.map((text, index) => (
+              <div key={index}>{text}</div>
+            ))}
+          </div>
           <div className="input-container">
             <input
               type="text"
@@ -37,11 +37,6 @@ const Philosophy = () => {
               placeholder="문장을 입력하세요"
             />
             <button onClick={handleAddText}>추가</button>
-          </div>
-          <div className="text-container">
-            {texts.map((text, index) => (
-              <div key={index}>{text}</div>
-            ))}
           </div>
         </div>
       </div>
