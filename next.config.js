f1b2be42638efca937f2ff2013b43d53245e0f73
minseob22 <minseobhan22@gmail.com>
2024-10-07 19:35:40 +0900

    배포를 위한 몸부림1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 // next.config.js
 module.exports = {
-    output: 'export',
-    images: {
-      unoptimized: true, // 이미지 최적화 비활성화
-    },
-  };
-  
+  output: 'export',
+  images: {
+    unoptimized: true, // 이미지 최적화 비활성화
+  },
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://minseob22.github.io/hahnAudioLab"
+      : "",
+};
